f8a683d908d8a43fc9347d399f37c24134f2ff82
Athena Henderson <henderson.athena@gmail.com>
2020-08-30 16:23:11 -0400

    descriptions copied

--- src/pages/services.js
+++ src/pages/services.js
@@ -9,27 +9,23 @@ const ServicesPage = ({data}) => (
     <SEO title="Services" />
     <section className="container content" style={{marginBottom: 20}}>
         <h1>Campus Services</h1>
+        <div class="alert alert-danger" role="alert"><b>Notice:</b> Due to the COVID-19 pandemic, all WMHD Radio events, open hours, services, and room rentals are canceled until further notice. The studio will be closed for this time period as well.</div >
         <p className="services-info">For any questions regarding our services or to book one of our studios, contact our officers by <a href="mailto:wmhdradio@gmail.com">email</a>.</p>
         <div className="row">
           <div className="col-md-4">
             <h3>DJ Services</h3>
             <Img fluid={data.djServices.childImageSharp.fluid} className="services-pic" />
-            <p>WMHD offers DJ services for on-campus events. If you would like us to DJ at your event, 
-            please fill out our rental request and DJing services <a href="https://docs.google.com/forms/d/e/1FAIpQLSeggZD4Busmn-YDP0NbEWqNMB_leOIePlblqkMUsb9WULST9g/viewform">form</a>. 
-            If DJ services are required, then WMHD Personnel will set up the equipment and play the requested music.</p>
+            <p>WMHD offers DJ services for on-campus events. A WMHD member will bring equipment and run it for any Rose-Hulman on-campus event for free. A playlist can either be created or a playlist can be provided by the event organizers. If you would like us to DJ at your event, please fill out our rental request and DJing services form. <a href="https://docs.google.com/forms/d/e/1FAIpQLSeggZD4Busmn-YDP0NbEWqNMB_leOIePlblqkMUsb9WULST9g/viewform">form</a> </p>
           </div>
           <div className="col-md-4">
             <h3>Equipment Rentals</h3>
             <Img fluid={data.equipmentRentals.childImageSharp.fluid} className="services-pic" />
-            <p>WMHD has equipment available to rent for any Rose-Hulman affiliated organization or student for free. 
-            There are speakers, microphones, lighting systems, mixers, and more that can be rented.
-            To request a rental, please fill out our rental request <a href="https://docs.google.com/forms/d/e/1FAIpQLSeggZD4Busmn-YDP0NbEWqNMB_leOIePlblqkMUsb9WULST9g/viewform">form</a>.</p>
+            <p>WMHD Radio offers free equipment rentals for any Rose-Hulman affiliated group or individual. Speakers, microphones, lightening systems, mixers, and more can all be provided. Popular events that request our equipment include concerts, karaoke, dances, and any event that would benefit from some music. We rent to events of all sizes and can help make your event a success! All rentals must be made at least one week in advance and renters will learn how to operate the equipment to make their event a hit! To request our equipment please fill out our services request form. <a href="https://docs.google.com/forms/d/e/1FAIpQLSeggZD4Busmn-YDP0NbEWqNMB_leOIePlblqkMUsb9WULST9g/viewform">form</a>.</p>
           </div>
           <div className="col-md-4">
             <h3>Recording Studio</h3>
             <Img fluid={data.recordingStudio.childImageSharp.fluid} className="services-pic" />
-            <p>Our Recording Studio is free to rent for recording or as a practice space by any students or groups on campus. 
-            The Recording Studio is equipped with a keyboard, drum kit, and bass amp, as well as plenty of microphones, music stands, and cables.</p>
+            <p>Our Recording Studio, located in BSB basement, is a soundproofed room that houses equipment and instruments that are available for any Rose-Hulman student to use. This fully functional recording studio can be used by musically aspirational students to record their art. Students don’t even need to own their own equipment as WMHD Radio owns several instruments that are available to the entire community! Music created using our equipment can be played on our station. To book our studio, send your emails to wmhdradio@gmail.com</p>
           </div>
         </div>
         <div className="row">
@@ -46,7 +42,7 @@ const ServicesPage = ({data}) => (
           <div className="col-md-4">
             <h3>Podcast Studio</h3>
             <Img fluid={data.podcastStudio.childImageSharp.fluid} className="services-pic" />
-            <p>Our Podcast Studio is free to book for recording any voice-based audio. There are high quality vocal microphones for 3 different speakers and more can be added if needed.</p>
+            <p>Our Production Studio is filled to the brim with technology both new and old. Housing everything from reel-to-reel players to soundboards this room has the equipment needed to make your own music. Connected to our Recording Studio, bands and musicians are able to record themselves and the mix the tracks in our Production Studio. Producers are also able to use our online tools such as FL Studio to create their own content. To book our studio, send your emails to wmhdradio@gmail.com</p>
           </div>
         </div>
     </section>
